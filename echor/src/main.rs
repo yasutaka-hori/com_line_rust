0137cf9a30c8cdfacf490613bd8e319a6f982fdc
Yasutaka Hori <yasutakahori95@gmail.com>
2025-08-10 11:02:09 +0900

    changed echor by using clap Derive API

--- echor/src/main.rs
+++ echor/src/main.rs
@@ -1,26 +1,23 @@
-use clap::{Arg, ArgAction, Command};
+use clap::Parser;
+
+#[derive(Debug, Parser)]
+#[command(author, version, about)]
+/// Rust version of `echo`
+struct Args {
+    /// Input text
+    #[arg(required(true))]
+    text: Vec<String>,
+
+    /// Do not print newline
+    #[arg(short('n'))]
+    omit_newline: bool,
+}
 
 fn main() {
-    let matches = Command::new("echor")
-        .version("0.1.0")
-        .author("Ken Youens-Clark <kyclark@gmail.com>")
-        .about("Rust echo")
-        .arg(
-            Arg::new("text")
-                .value_name("TEXT")
-                .help("Input text")
-                .required(true)
-                .num_args(1..),
-        )
-        .arg(
-            Arg::new("omit_newline")
-                .short('n')
-                .action(ArgAction::SetTrue)
-                .help("Do not print newline"),
-        )
-        .get_matches();
-
-    let text: Vec<String> = matches.get_many("text").unwrap().cloned().collect();
-    let omit_newline = matches.get_flag("omit_newline");
-    println!("{}{}", text.join(" "), if omit_newline { "" } else { "\n" });
+    let args = Args::parse();
+    print!(
+        "{}{}",
+        args.text.join(" "),
+        if args.omit_newline { "" } else { "\n" }
+    );
 }
