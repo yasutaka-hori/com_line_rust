5878dd5e4f80e668445e7bd6963511cb39cf391b
Yasutaka Hori <yasutakahori95@gmail.com>
2025-09-01 18:21:29 +0900

    update: delete target dir

--- echor/tests/cli.rs
+++ echor/tests/cli.rs
@@ -1,53 +1,50 @@
-use anyhow::Result;
 use assert_cmd::Command;
 use predicates::prelude::*;
-use pretty_assertions::assert_eq;
 use std::fs;
+
+type TestResult = Result<(), Box<dyn std::error::Error>>;
 
 // --------------------------------------------------
 #[test]
-fn dies_no_args() -> Result<()> {
+fn dies_no_args() -> TestResult {
     Command::cargo_bin("echor")?
         .assert()
         .failure()
-        .stderr(predicate::str::contains("Usage"));
+        .stderr(predicate::str::contains("USAGE"));
     Ok(())
 }
 
 // --------------------------------------------------
-fn run(args: &[&str], expected_file: &str) -> Result<()> {
+fn run(args: &[&str], expected_file: &str) -> TestResult {
     let expected = fs::read_to_string(expected_file)?;
-    let output = Command::cargo_bin("echor")?
+    Command::cargo_bin("echor")?
         .args(args)
-        .output()
-        .expect("fail");
-
-    let stdout = String::from_utf8(output.stdout).expect("invalid UTF-8");
-    assert_eq!(stdout, expected);
-
+        .assert()
+        .success()
+        .stdout(expected);
     Ok(())
 }
 
 // --------------------------------------------------
 #[test]
-fn hello1() -> Result<()> {
+fn hello1() -> TestResult {
     run(&["Hello there"], "tests/expected/hello1.txt")
 }
 
 // --------------------------------------------------
 #[test]
-fn hello2() -> Result<()> {
+fn hello2() -> TestResult {
     run(&["Hello", "there"], "tests/expected/hello2.txt")
 }
 
 // --------------------------------------------------
 #[test]
-fn hello1_no_newline() -> Result<()> {
+fn hello1_no_newline() -> TestResult {
     run(&["Hello  there", "-n"], "tests/expected/hello1.n.txt")
 }
 
 // --------------------------------------------------
 #[test]
-fn hello2_no_newline() -> Result<()> {
+fn hello2_no_newline() -> TestResult {
     run(&["-n", "Hello", "there"], "tests/expected/hello2.n.txt")
 }
